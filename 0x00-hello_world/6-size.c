9e34ead25b7b69647d5bb806118ef41e54e28245
elothamni1993 <elothmaniotmane00@gmail.com>
2023-06-18 14:23:01 -0700

    test

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+
+int main(void)
+{
+	int n;
+	int i = 0;
+
+	for (n = 0 ; n < 100 ; n++)
+	{
+		putchar(n / 10 + '0');
+		putchar(i + '0');
+		if (n < 99)
+		{
+			putchar(',');
+			putchar(32);
+		}
+		i++;
+		if (i > 9)
+		{
+			i = 0;
+		}
+	}
+	putchar('\n');
+	return (0);
+}

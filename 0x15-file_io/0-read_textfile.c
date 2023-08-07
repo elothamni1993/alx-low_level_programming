7c25426db223fa02bd0a067766adeca48f3c1e8a
elothamni1993 <elothmaniotmane00@gmail.com>
2023-08-07 07:46:11 -0700

    otmane

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,47 +1,32 @@
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <fcntl.h>
-#include <unistd.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
 
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-    int fd;
-    ssize_t read_bytes, write_bytes;
-    char *buffer;
+	ssize_t file, let, w;
+	char *text;
 
-    if (!filename)
-        return (0);
+	text = malloc(letters);
+	if (text == NULL)
+		return (0);
 
-    fd = open(filename, O_RDONLY);
-    if (fd == -1)
-        return (0);
+	if (filename == NULL)
+		return (0);
 
-    buffer = malloc(sizeof(char) * letters);
-    if (!buffer)
-    {
-        close(fd);
-        return (0);
-    }
+	file = open(filename, O_RDONLY);
 
-    read_bytes = read(fd, buffer, letters);
-    if (read_bytes == -1)
-    {
-        free(buffer);
-        close(fd);
-        return (0);
-    }
+	if (file == -1)
+	{
+		free(text);
+		return (0);
+	}
 
-    write_bytes = write(STDOUT_FILENO, buffer, read_bytes);
-    if (write_bytes == -1 || write_bytes != read_bytes)
-    {
-        free(buffer);
-        close(fd);
-        return (0);
-    }
+	let = read(file, text, letters);
 
-    free(buffer);
-    close(fd);
-    return (write_bytes);
+	w = write(STDOUT_FILENO, text, let);
+
+	close(file);
+
+	return (w);
 }
-

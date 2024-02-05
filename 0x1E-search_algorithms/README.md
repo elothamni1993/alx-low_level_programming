54bb10a944e252dbbeb3a4a1c8e3cfe8118be94a
the0zero0 <cryptologia6@gmail.com>
2024-02-05 17:07:54 +0100

    algo

---
+++ 0x1E-search_algorithms/README.md
@@ -0,0 +1,5 @@
+0x1E-search_algorithms
+
+0x1E-search_algorithms
+
+
---
+++ 0x1E-search_algorithms/search_algos.h
@@ -0,0 +1,17 @@
+#ifndef SEARCH_H
+#define SEARCH_H
+
+#include <stdio.h>
+
+/* Prototypes*/
+int linear_search(int *array, size_t size, int value);
+int binary_search(int *array, size_t size, int value);
+#endif#ifndef SEARCH_H
+#define SEARCH_H
+
+#include <stdio.h>
+
+/* Prototypes*/
+int linear_search(int *array, size_t size, int value);
+int binary_search(int *array, size_t size, int value);
+#endif
